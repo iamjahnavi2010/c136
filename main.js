5123e89dc77acf19f4237ec3b302ecf5e18c8480
iamjahnavi2010 <78913699+iamjahnavi2010@users.noreply.github.com>
2022-03-18 17:28:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 video = "";
+objects = "";
 status = "";
 
 function preload(){
@@ -13,6 +14,20 @@ function setup() {
 
 function draw() {
     image(video, 0, 0,480, 360);
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResult);
+        for(i=0; i<objects.length; i++) {
+            document.getElementById("status").innerHTML ="Status : Object Dectected";
+            document.getElementById("number_of_objects").innerHTML ="Number of objects dectected are : "+ objects.length;
+            fill('#0000ff');
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x +15 ,objects[i].y+15);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function start(){
@@ -27,3 +42,10 @@ function modelLoaded(){
     video.speed(1);
     video.volume(0);
 } 
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
